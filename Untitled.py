330aa54dea7f3cbfd8f28da0525c0aeec6bfcce5
Yiyin1997 <904993511@qq.com>
2024-09-21 19:37:37 +0800

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import streamlit as st  
+import os  
+  
+# 获取当前工作目录  
+current_dir = os.getcwd()  
+  
+# 在Streamlit中显示当前工作目录  
+st.write("当前工作目录是：", current_dir)
+
+
+# In[ ]:
+
+
+
+
